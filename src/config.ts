097035c02c6b255f2bc88ff843efc2756a565d1a
zMr2010 <15336889926@163.com>
2026-09-03 20:21:14 +0800

    new avatar

--- src/config.ts
+++ src/config.ts
@@ -67,7 +67,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "/images/Avatar_Evanescia.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "/images/avatar_music.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "XianRuiDendro",
 	bio: "Dispel the gloom and restore clear skies once more!",
 	links: [
